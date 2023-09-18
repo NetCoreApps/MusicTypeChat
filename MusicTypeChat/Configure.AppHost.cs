e063e230cbcaf4ea2f7658cfaab9f1573b6fad4c
Demis Bellot <demis.bellot@gmail.com>
2023-09-19 03:02:14 +0800

    Update to latest v6.10.1

--- MusicTypeChat/Configure.AppHost.cs
+++ MusicTypeChat/Configure.AppHost.cs
@@ -103,13 +103,4 @@ public class AppHost : AppHostBase, IHostingStartup
 
         return to;
     }
-
-    public static void AssertGoogleCloudCredentials()
-    {
-        var googleCredentials = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
-        if (string.IsNullOrEmpty(googleCredentials))
-            throw new Exception("GOOGLE_APPLICATION_CREDENTIALS Environment Variable not set");
-        if (!File.Exists(googleCredentials))
-            throw new Exception($"GOOGLE_APPLICATION_CREDENTIALS '{googleCredentials}' does not exist");
-    }
 }
